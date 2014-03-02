f6761d169b46e623d1f9f9cc2dcf970ef421fc5d
Joris Vankerschaver <jvankerschaver@enthought.com>
2014-03-02 15:06:37 +0000

    Checker board editor demo.

---
+++ patterns/demo/checker_board_demo.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from traits.etsconfig.api import ETSConfig
+ETSConfig.toolkit = 'qt4'
+
+from traits.api import Array, HasTraits
+from traitsui.api import Item, View
+
+from patterns.checker_board import CheckerBoardEditor
+
+
+class MainWindow(HasTraits):
+
+    array = Array
+
+    view = View(
+        Item('array', editor=CheckerBoardEditor()),
+        resizable=True
+    )
+
+    def _array_default(self):
+        return np.array([[False, True], [True, False], [True, True]])
+
+
+if __name__ == '__main__':
+    main = MainWindow()
+    main.configure_traits()
