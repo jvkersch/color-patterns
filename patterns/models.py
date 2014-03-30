7025a386ab7c52cd8d9460285d7ebad0256fa966
Joris Vankerschaver <jvankerschaver@enthought.com>
2014-03-30 15:31:25 +0100

    Update checker board in response to change in dimensions.

--- patterns/models.py
+++ patterns/models.py
@@ -6,7 +6,7 @@ Simple facades around checkerboard models, adapted for various purposes.
 
 import numpy as np
 
-from traits.api import Array, HasTraits, Int
+from traits.api import Array, HasTraits, Int, on_trait_change
 
 
 class Binding(HasTraits):
@@ -25,6 +25,16 @@ class Binding(HasTraits):
 
     def _columns_default(self):
         return self.array.shape[1]
+
+    @on_trait_change('rows, columns')
+    def _update_array_size(self):
+        # TODO Refactor this?
+        new_array = np.zeros((self.rows, self.columns), dtype=bool)
+        old_rows, old_columns = self.array.shape
+        rows = min(old_rows, self.rows)
+        columns = min(old_columns, self.columns)
+        new_array[:rows, :columns] = self.array[:rows, :columns]
+        self.array = new_array
 
 
 class Pattern(HasTraits):
