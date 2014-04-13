f2e6b6235737faa797b2a9d8f54032df4306123c
Joris Vankerschaver <jvankerschaver@enthought.com>
2014-04-13 07:47:41 +0100

    Updated adjustable editor demo.

--- patterns/demo/adjustable_checker_board_demo.py
+++ patterns/demo/adjustable_checker_board_demo.py
@@ -8,15 +8,16 @@ import numpy as np
 from traits.etsconfig.api import ETSConfig
 ETSConfig.toolkit = 'qt4'
 
-from traits.api import Array, HasTraits, Int, on_trait_change
+from traits.api import HasTraits, Instance, Int, on_trait_change
 from traitsui.api import HGroup, VGroup, Item, View
 
 from patterns.checker_board import CheckerBoardEditor
+from patterns.checker_board_model import CheckerBoardModel
 
 
 class MainWindow(HasTraits):
 
-    array = Array
+    board = Instance(CheckerBoardModel)
 
     rows = Int
 
@@ -30,28 +31,25 @@ class MainWindow(HasTraits):
                 label='Array Dimensions',
                 show_border=True,
             ),
-            Item('array', editor=CheckerBoardEditor()),
+            Item('board', editor=CheckerBoardEditor()),
         ),
         resizable=True
     )
 
-    def _array_default(self):
-        return np.array([[False, True], [True, False], [True, True]])
+    def _board_default(self):
+        data = np.array([[False, True], [True, False], [True, True]])
+        return CheckerBoardModel(data=data)
 
     def _rows_default(self):
-        return self.array.shape[0]
+        return self.board.rows
 
     def _columns_default(self):
-        return self.array.shape[1]
+        return self.board.columns
 
     @on_trait_change('rows, columns')
     def _update_array_size(self):
-        new_array = np.zeros((self.rows, self.columns), dtype=bool)
-        old_rows, old_columns = self.array.shape
-        rows = min(old_rows, self.rows)
-        columns = min(old_columns, self.columns)
-        new_array[:rows, :columns] = self.array[:rows, :columns]
-        self.array = new_array
+        self.board.columns = self.columns
+        self.board.rows = self.rows
 
 
 if __name__ == '__main__':
