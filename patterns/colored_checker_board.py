2017d73af331ff3f87fb068076994e2943e63a4c
Joris Vankerschaver <jvankerschaver@enthought.com>
2014-12-30 09:23:23 +0000

    Colors from colorbars.

--- patterns/colored_checker_board.py
+++ patterns/colored_checker_board.py
@@ -23,20 +23,51 @@ class ColorSelectTool(BaseTool):
         component = self.component
         model = component.model
 
-        try:
-            mapped_index = model.screen_to_array_index(x, y)
-        except ValueError:
-            pass
-        else:
-            component.select_color(*mapped_index)
+        mapped = model.screen_to_color_index(x, y)
+        if mapped is not None:
+            index, is_horizontal = mapped
+            component.select_color(index, is_horizontal)
 
 
 class ColoredCheckerBoardComponent(CheckerBoardComponent):
 
     def _draw(self, gc, view_bounds=None, mode="default"):
-
+        self._draw_color_bars(gc)
         self._draw_colors(gc)
         self._draw_outline(gc)
+
+    def _draw_color_bars(self, gc):
+
+        model = self.model
+        x_coords = model._x_coords
+        y_coords = model._y_coords
+        top, bottom, left, right = model._padding_abs
+
+        with gc:
+            gc.set_line_width(2.0)
+            gc.set_stroke_color((0.0, 0.0, 0.0))
+
+            for x in x_coords:
+                # Vertical strokes
+                gc.move_to(x, bottom)
+                gc.line_to(x, bottom - 15)
+                gc.stroke_path()
+
+            # Top and bottom line
+            gc.move_to(x_coords[0], bottom - 15)
+            gc.line_to(x_coords[-1], bottom - 15)
+            gc.stroke_path()
+
+            for y in y_coords:
+                # Horizontal strokes
+                gc.move_to(left, y)
+                gc.line_to(left - 15, y)
+                gc.stroke_path()
+
+            # Left-most line
+            gc.move_to(left - 15, y_coords[0])
+            gc.line_to(left - 15, y_coords[-1])
+            gc.stroke_path()
 
     def _draw_colors(self, gc):
 
@@ -45,27 +76,51 @@ class ColoredCheckerBoardComponent(CheckerBoardComponent):
         x_coords = model._x_coords
         y_coords = model._y_coords
         cx, cy = model._cell_size
+        top, bottom, left, right = model._padding_abs
 
         with gc:
-            for i in range(nx):
-                for j in range(ny):
-                    color = self.model.colors[ny - 1 - j, i]
+            # Horizontal color bar.
+            for x, color in zip(x_coords, model.hor_colors):
+                gc.set_fill_color(color)
+                gc.draw_rect((x, bottom - 15, cx, 15), FILL)
+
+            # Vertical color bar.
+            for y, color in zip(y_coords, model.ver_colors[::-1]):
+                gc.set_fill_color(color)
+                gc.draw_rect((left - 15, y, 15, cy), FILL)
+
+            # Colors in the interior.
+            for i in xrange(nx):
+                for j in xrange(ny):
+                    if model.data[ny - j - 1, i]:  # Dotted square.
+                        color = model.hor_colors[i]
+                    else:
+                        color = model.ver_colors[ny - j - 1]
+
                     gc.set_fill_color(color)
                     gc.draw_rect(
                         (x_coords[i], y_coords[j], cx, cy),
-                        FILL)
+                        FILL
+                    )
 
-    def select_color(self, i, j):
+    def select_color(self, index, is_horizontal):
         """
-        Display a color dialog box to select a color for square (i, j).
+        Display a color dialog box to select a new color.
 
         """
+        if is_horizontal:
+            color_array = self.model.hor_colors
+        else:
+            color_array = self.model.ver_colors
+            # Colors are laid out from top to bottom.
+            # index = len(color_array) - index - 1
+
         color = QtGui.QColorDialog.getColor()
         if color.isValid():
             rgb_color = rgb_from_qt_color(color)
-            self.model.colors[i, j] = rgb_color
+            color_array[index] = rgb_color
             print "Selected color {} for box {}.".format(
-                rgb_color, (i, j))
+                rgb_color, (index, is_horizontal))
             self.request_redraw()
 
     def _tools_default(self):
--- patterns/colored_checker_board_model.py
+++ patterns/colored_checker_board_model.py
@@ -1,3 +1,4 @@
+from math import floor
 import numpy as np
 
 from traits.api import Array, on_trait_change
@@ -10,18 +11,55 @@ COLOR_DTYPE = np.dtype([('red', float), ('green', float), ('blue', float)])
 
 class ColoredCheckerBoardModel(CheckerBoardModel):
 
-    colors = Array
+    hor_colors = Array
 
-    def _colors_default(self):
-        arr = np.empty(self.data.shape, dtype=COLOR_DTYPE)
+    ver_colors = Array
+
+    def _hor_colors_default(self):
+        arr = np.empty(self.data.shape[1], dtype=COLOR_DTYPE)
+        arr.fill((1.0, 1.0, 1.0))
+        return arr
+
+    def _ver_colors_default(self):
+        arr = np.empty(self.data.shape[0], dtype=COLOR_DTYPE)
         arr.fill((1.0, 1.0, 1.0))
         return arr
 
     @on_trait_change('data')
     def _update_colors_shape(self):
 
-        cx, cy = [min(*t) for t in zip(self.colors.shape, self.data.shape)]
-        new_colors = np.empty(self.data.shape, dtype=COLOR_DTYPE)
-        new_colors.fill((1.0, 1.0, 1.0))
-        new_colors[:cx, :cy] = self.colors[:cx, :cy]
-        self.colors = new_colors
+        # Horizontal colors
+        l = min(self.hor_colors.shape[0], self.data.shape[1])
+        new = np.empty(self.data.shape[1], dtype=COLOR_DTYPE)
+        new.fill((1.0, 1.0, 1.0))
+        new[:l] = self.hor_colors[:l]
+        self.hor_colors = new
+
+        # Vertical colors
+        l = min(self.ver_colors.shape[0], self.data.shape[0])
+        new = np.empty(self.data.shape[0], dtype=COLOR_DTYPE)
+        new.fill((1.0, 1.0, 1.0))
+        new[:l] = self.ver_colors[:l]
+        self.ver_colors = new
+
+    def screen_to_color_index(self, x, y):
+        """ Translate (x, y) to an index in one of the color arrays.
+
+        Returns
+        -------
+        index, is_horizontal or None.
+
+        """
+        ny, nx = self.data.shape
+        cx, cy = self._cell_size
+        _, bottom, left, _ = self._padding_abs
+
+        i = int(floor((x - left) / cx))
+        j = int(ny - 1 - floor((y - bottom) / cy))
+
+        if j == ny:
+            if 0 <= i < nx:
+                return i, True
+        elif i == -1:
+            if 0 <= j < ny:
+                return j, False
